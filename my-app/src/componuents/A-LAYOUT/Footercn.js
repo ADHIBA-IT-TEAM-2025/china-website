1d0f82eb002e4b48d154d9072a52440bfca06051
SANJEEV <sanjeevshanmugam02@gmail.com>
2024-08-24 10:46:33 +0530

    some changes

--- my-app/src/componuents/A-LAYOUT/Footercn.js
+++ my-app/src/componuents/A-LAYOUT/Footercn.js
@@ -1,46 +1,144 @@
 import React from "react";
 import './nav.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faTelegramPlane, faWhatsapp, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faPhone } from "@fortawesome/free-solid-svg-icons/faPhone";
-
+import logo2 from '../Z-IMAGE/nystai without tri.png';  // Import the second logo
+import { faBlog, faEnvelope, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons/faMapMarkedAlt";
 
 
 export default function Footercn() {
     return (
         <>
-            <div class=" pt-5 d-flex justify-content-center text-center" style={{ color: "#EBF5EE", backgroundColor: "#00101a" }}>
-                <div class="a">
-                    <footer class="row footer">
-                        <div class="col">
-                            <h2 class="d-flex align-items-center justify-content-center" style={{ fontWeight: "700;" }}>Nystai - Leading AIoT Security Solutions</h2>
-                            <div class="soc mt-4 mb-3">
-                                <a>SUPPLY CHAIN MANAGEMENT</a>
-                                <a>TECHNOLOGIES</a>
+
+            <footer class="footer-section">
+                <div class="container">
+                    <div class="footer-cta pt-5 ">
+                        <div class="row">
+                           
+                            <div class="col-xl-4 col-md-4 mb-30">
+                                <div class="single-cta">
+                                    <FontAwesomeIcon icon={faMapMarkedAlt} className="custom-icon" />
+                                    <div class="cta-text">
+                                        <h4>Find us</h4>
+                                        <span>Akshya Nagar 1st Block 1st Cross,China-560016 </span>
+                                   </div>
+                                </div>
                             </div>
-                            <div class="soc mt-4 mb-3">
-                                <a><FontAwesomeIcon icon={faWhatsapp} /></a>
-                                <a><FontAwesomeIcon icon={faInstagram} /></a>
-                                <a><FontAwesomeIcon icon={faFacebook} /></a>
-                                <a ><FontAwesomeIcon icon={faPhone} /></a>
-                                <a ><FontAwesomeIcon icon={faTwitter} /></a>
-                                <a ><FontAwesomeIcon icon={faXTwitter} /></a>
-                                <a ><FontAwesomeIcon icon={faYoutube} /></a>
-                                <a ><FontAwesomeIcon icon={faLinkedin} /></a>
+                            <div class="col-xl-4 col-md-4 mb-30">
+                                <div class="single-cta">
+                                    <FontAwesomeIcon icon={faPhone} className="custom-icon" />
+                                    <div class="cta-text">
+                                        <h4>Call us</h4>
+                                        <span>+91 1234567890</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-md-4 mb-30">
+                                <div class="single-cta">
+                                    <FontAwesomeIcon icon={faEnvelope} className="custom-icon" />
+                                    <div class="cta-text">
+                                        <h4>Mail us</h4>
+                                        <span>contact@gmail.com</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </footer>
-                    <div class="legal mt-2">
-                        <p> SHENZHEN NYSTAI TECHNOLOGY COMPANY LIMITED</p>
                     </div>
-                    <br />
+                    <div class="footer-content pt-5 pb-1">
+                        <div class="row">
+                            <div class="col-xl-4 col-lg-4 mb-50">
+                                <div class="footer-widget">
+                                    <div class="footer-widget-heading">
+                                        <h3>Discription</h3>
+                                    </div>
+                                    <div class="footer-text mt-3">
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                                    </div>
+                                    <div class="footer-social-icon">
+                                        <span>Follow us</span>
+                                        <ul class="social_icon">
+                                            <p><FontAwesomeIcon icon={faInstagram} style={{ color: '#E1306C' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faWhatsapp} style={{ color: '#25D366' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faYoutube} style={{ color: '#FF0000' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faTelegramPlane} style={{ color: '#0088CC' }} className="ms-3 " /></p>
+                                            <p><FontAwesomeIcon icon={faXTwitter} style={{ color: '#fff' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faBlog} style={{ color: '#000' }} className="ms-3" /></p>
+                                            <p><FontAwesomeIcon icon={faEnvelope} style={{ color: '#3e65cf' }} className="ms-3" /></p>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                                <div class="footer-widget">
+                                    <div class="footer-widget-heading">
+                                        <h3>Useful Links</h3>
+                                    </div>
+                                    <ul>
+                                        <li><a href="#">Our Team</a></li>
+                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="#">Our Gallery</a></li>
+                                        <li><a href="#">Selection Process</a></li>
+                                        <li><a href="#">FAQ</a></li>
+                                        <li><a href="#">Sponsorship</a></li>
+                                        <li><a href="#">Our Policies</a></li>
+                                        <li><a href="#">Our Team</a></li>
+                                        <li><a href="#">Contact us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-lg-4 col-md-6 ">
+                                <div class="footer-widget">
+                                    <div class="footer-widget-heading">
+                                        <h3>Subscribe</h3>
+                                    </div>
+                                    <div class="footer-text mb-25">
+                                        <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr className=" " />
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                                <div class="copyright-text">
+                                    <p>SHENZHEN NYSTAI TECHNOLOGY COMPANY LIMITED</p>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                                <div class="footer-menu">
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Terms</a></li>
+                                        <li><a href="#">Privacy</a></li>
+                                        <li><a href="#">Policy</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
-            </div>
+
+            </footer>
+
+
+
         </>
     )
 }
--- my-app/src/componuents/A-LAYOUT/layoutchina.js
+++ my-app/src/componuents/A-LAYOUT/layoutchina.js
@@ -1,65 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import './nav.css';
-import trinav from '../Z-IMAGE/triangle (6).png';
 import logo1 from '../Z-IMAGE/triangle (6).png';  // Import the first logo
 import logo2 from '../Z-IMAGE/nystai without tri.png';  // Import the second logo
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser, faEarthAmericas, faLanguage } from "@fortawesome/free-solid-svg-icons";
 
 const Layoutchinna = () => {
-    const [navActive, setNavActive] = useState(false);
-    const [navOpen, setNavOpen] = useState(false);
-    const [scrollY, setScrollY] = useState(0);
+    const [active, setActive] = useState(false);
+    const [hoveredItem, setHoveredItem] = useState(null); // State to track hovered item
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrollY(window.scrollY);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    const openNav = () => {
-        setNavOpen(true);
+    const menuToggle = () => {
+        setActive(!active);
     };
 
-    const closeNav = () => {
-        setNavOpen(false);
+    // Map each menu item to a specific image URL
+    const images = {
+        Home: "https://images.unsplash.com/photo-1453989799106-bbad8d7b5191?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80",
+        SOLUTION: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+        PRODUCT: "https://images.unsplash.com/photo-1506702315536-dd8b83e2dcf9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+        SERVICES: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+        PROTECTPLAN: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+        Contact: "https://images.unsplash.com/photo-1506702315536-dd8b83e2dcf9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
     };
 
     return (
         <>
-            <nav className="nav navbar navbar-china-cn sticky-top">
-                <div className="container">
-                    <div className="navbar-brand">
-                        <div>
-                            <div id="myNav" className={`overlay ${navOpen ? 'open' : ''}`}>
-                                <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
-                                <div className="overlay-content">
-                                    <a href="#">About</a>
-                                    <a href="#">Services</a>
-                                    <a href="#">Clients</a>
-                                    <a href="#">Contact</a>
-                                </div>
-                            </div>
-                        
-                            <span style={{ cursor: "pointer" }} onClick={openNav}>&#9776; MENU</span>
-                        </div>
-                    </div>
-                    <ul className={`navbar-nav ${navActive ? 'active' : ''}`}>
-                    <img src={scrollY === 0 ? logo1 : logo2} alt="Logo" className="navbar-logo" /> 
-                    </ul>
-                    <button className="mode-toggle-container" id="mode-toggle">
-                        <FontAwesomeIcon icon={faCircleUser} shake className="me-3"/> 
-                        <FontAwesomeIcon icon={faEarthAmericas} />
-                    </button>
+            <div className={`fullPageMenu ${active ? 'active' : ''}`} id="nav">
+                <div className="banner">
+                    {/* Display the image based on the hovered item */}
+                    <img
+                        src={hoveredItem ? images[hoveredItem] : images.Home}
+                        alt="banner"
+                    />
                 </div>
-            </nav>
-            
+                <div className="nav">
+                    <ul>
+                        {/* Assign hover handlers to each navigation link */}
+                        {Object.keys(images).map((item) => (
+                            <li key={item}>
+                                <a
+                                    href="#"
+                                    data-text={item}
+                                    onMouseEnter={() => setHoveredItem(item)}
+                                    onMouseLeave={() => setHoveredItem(null)}
+                                >
+                                    {item}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <span
+                    className={`menuicon ${active ? 'active' : ''}`}
+                    id="toggle"
+                    onClick={menuToggle}
+                ></span>
+            </div>
+
             <Outlet />
         </>
     );
